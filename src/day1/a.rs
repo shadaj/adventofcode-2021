7953c28b7da6598271b86ca6d3b47ad2ac70836c
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-12-02 13:26:45 -0800

    Dedup util

--- src/day1/a.rs
+++ src/day1/a.rs
@@ -1,59 +1,10 @@
 // BEGIN UTIL (https://codeforces.com/blog/entry/67391)
-#[allow(dead_code)]
-mod util {
-    use std::{io::stdin, str::FromStr};
-
-    pub struct ScannerManyIterator<'a, T: FromStr> {
-        scanner: &'a mut Scanner,
-        phantom: std::marker::PhantomData<T>,
-    }
-
-    impl<'a, T: FromStr> Iterator for ScannerManyIterator<'a, T> {
-        type Item = T;
-
-        fn next(&mut self) -> Option<T> {
-            self.scanner.next()
-        }
-    }
-
-    pub struct Scanner {
-        buffer: Vec<String>,
-    }
-
-    impl Scanner {
-        pub fn new() -> Scanner {
-            Scanner { buffer: Vec::new() }
-        }
-
-        pub fn next<T: std::str::FromStr>(&mut self) -> Option<T> {
-            loop {
-                if let Some(token) = self.buffer.pop() {
-                    return Some(token.parse().ok().expect("Failed parse"));
-                }
-
-                let mut input = String::new();
-
-                stdin().read_line(&mut input).expect("Failed read");
-                if input.len() == 0 {
-                    return None;
-                } else {
-                    self.buffer = input.split_whitespace().rev().map(String::from).collect();
-                }
-            }
-        }
-
-        pub fn many<'a, T: std::str::FromStr>(&'a mut self) -> ScannerManyIterator<'a, T> {
-            ScannerManyIterator {
-                scanner: self,
-                phantom: std::marker::PhantomData,
-            }
-        }
-    }
-}
+#[path = "../util.rs"]
+mod util;
 // END UTIL
 
+use std::io::{stdout, BufWriter, Write};
 use util::*;
-use std::io::{Write, BufWriter, stdout};
 
 fn main() {
     let mut input = Scanner::new();
